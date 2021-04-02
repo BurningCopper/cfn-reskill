809e02b30c20fec8b2654faefa8aac34b59c716b
BurningCopper <78751061+BurningCopper@users.noreply.github.com>
2021-04-02 13:53:40 -0500

    Add files via upload

---
+++ scripts/create-stack.sh
@@ -0,0 +1,1 @@
+aws cloudformation create-stack --stack-name cfn-reskill-stack --template-body file://../templates/template.yaml
---
+++ scripts/update-stack.sh
@@ -0,0 +1,1 @@
+aws cloudformation update-stack --stack-name cfn-reskill-stack --template-body file://../templates/template.yaml
